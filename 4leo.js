a3269b5f50dec4cf3d388506f2087e9cf5839ed7
Alexandre Karpov <alexakarpov@gmail.com>
2021-04-23 22:18:07 -0400

    move test data to spec file

--- 4leo.js
+++ 4leo.js
@@ -1,11 +1,3 @@
-var data = [
-    [['key11', 'value11'], ['key12','value12'],['key13', 'value13']],
-    [['key21', 'value21'], ['key22','value22'],['key23', 'value23']],
-    [['key31', 'value31'], ['key32','value32'],['key33', 'value33']]
-];
-
-exports.data = data;
-
 exports.transformEmployeeData = function(employeeData) {
     pairsToObj = function(pairs) {
         var obj = {};
